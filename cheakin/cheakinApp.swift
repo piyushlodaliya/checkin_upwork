151b8637cbff424b4333889c6b613435a6c1c116
Arnav Gupta <strarnv@gmail.com>
2025-09-29 16:12:41 -0700

    Initial Commit

---
+++ cheakin/cheakinApp.swift
@@ -0,0 +1,17 @@
+//
+//  cheakinApp.swift
+//  cheakin
+//
+//  Created by Arnav Gupta on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct cheakinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
